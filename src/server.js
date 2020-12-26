5f57a044a127f3bbc07f45f62dc816a7c054f006
chof747 <chof@gmx.at>
2020-12-26 23:06:08 +0100

    Deleted old source

--- src/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const app = require('./app');
-//const config = require("./config");
- 
-const port = process.env.PORT || 80;
- 
-const server = http.createServer(app);
- 
-server.listen(port);
