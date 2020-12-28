52b6077c442018cc7555e95288e0b124e8109f3b
chof747 <chof@gmx.at>
2020-12-28 15:42:21 +0100

    removed boilerplate model and added device model

--- src/migrations/20201228134410-create-device.js
+++ src/migrations/20201228134410-create-device.js
@@ -1,20 +1,23 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('Devices', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      firstName: {
+      name: {
         type: Sequelize.STRING
       },
-      lastName: {
+      mac: {
         type: Sequelize.STRING
       },
-      email: {
+      description: {
+        type: Sequelize.STRING
+      },
+      model: {
         type: Sequelize.STRING
       },
       createdAt: {
@@ -28,6 +31,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('Devices');
   }
 };
--- src/models/device.js
+++ src/models/device.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Device extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,13 +13,20 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   };
-  User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING
+  Device.init({
+    name: DataTypes.STRING,
+    mac: {
+      type : DataTypes.STRING,
+      is: {
+        args : "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$",
+        msg : "mac must be a valid MAC address XX:XX:XX:XX:XX:XX"
+      }
+    },
+    description: DataTypes.STRING,
+    model: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'User',
+    modelName: 'Device',
   });
-  return User;
+  return Device;
 };
